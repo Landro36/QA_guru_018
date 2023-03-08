f190da2d2d4c28bc2b45967807baaa8aa815407c
landr <landroshop@gmail.com>
2023-03-08 17:25:13 +0300

    TestGitHub

---
+++ src/test/java/TestGitHub.java
@@ -0,0 +1,44 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestGitHub {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.browserSize="1920x1080";
+        Configuration.baseUrl="https://github.com";
+    }
+
+    @Test
+    void checkExampleCode() {
+
+        String exampleCodeForJUnit5 = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+
+        //Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".markdown-body").shouldHave(text(exampleCodeForJUnit5));
+    }
+
+}
